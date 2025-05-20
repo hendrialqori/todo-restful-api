166ebe952030f7a8fe5f296c397972af6b24be3d
Hendri Alqori <teamhendri18@gmail.com>
2025-05-20 17:21:55 +0700

    feat: add database connection

--- app/database.go
+++ app/database.go
@@ -1,5 +1,25 @@
 package app
 
-func DB() {
+import (
+	"database/sql"
+	"time"
+)
 
+func NewDB() *sql.DB {
+	var (
+		driver = "mysql"
+		dsn    = "root:root@(localhost:3306)/todo_api?parseTime=true"
+	)
+
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(2 * time.Hour)
+	db.SetConnMaxIdleTime(30 * time.Minute)
+
+	return db
 }
