4eb23ee90f957f0642f94245cc8c5857d6771a06
Hendri Alqori <teamhendri18@gmail.com>
2025-05-20 17:26:02 +0700

    feat: create role response model

---
+++ internal/model/web/role_response.go
@@ -0,0 +1,8 @@
+package web
+
+type RoleResponse struct {
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
