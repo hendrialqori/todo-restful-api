77058968eed083b9abca39dbeec8bd2229f6ac13
Hendri Alqori <teamhendri18@gmail.com>
2025-05-21 17:32:02 +0700

    feat: create user routing api

---
+++ internal/route/user_route.go
@@ -0,0 +1,26 @@
+package route
+
+import (
+	"database/sql"
+	controller "todo-restful-api/internal/controller/user"
+	repository "todo-restful-api/internal/repository/user"
+	service "todo-restful-api/internal/service/user"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+)
+
+func UserRouter(router *httprouter.Router, DB *sql.DB, validate *validator.Validate) {
+
+	var (
+		userRepository = repository.NewUserRepository(DB)
+		userService    = service.NewUserService(userRepository, validate)
+		userController = controller.NewUserController(userService)
+	)
+
+	router.GET("/api/users", userController.FindAll)
+	router.GET("/api/users/:userId", userController.FindById)
+	router.POST("/api/users", userController.Create)
+	router.PUT("/api/users/:userId", userController.Update)
+	router.DELETE("/api/users/:userId", userController.Delete)
+}
