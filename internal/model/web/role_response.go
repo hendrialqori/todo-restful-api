aeee99599ba4c31df7672e17794c3aa511b8665d
Hendri Alqori <teamhendri18@gmail.com>
2025-05-21 17:33:52 +0700

    feat: add omitempty to RoleResponse struct

--- internal/model/web/role_response.go
+++ internal/model/web/role_response.go
@@ -3,6 +3,6 @@ package web
 type RoleResponse struct {
 	Id        int    `json:"id"`
 	Name      string `json:"name"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	CreatedAt string `json:"created_at,omitempty"`
+	UpdatedAt string `json:"updated_at,omitempty"`
 }
