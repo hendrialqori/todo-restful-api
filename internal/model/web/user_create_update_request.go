ee8759ba239868b15a40f0badc76489795e8587f
Hendri Alqori <teamhendri18@gmail.com>
2025-05-21 17:34:19 +0700

    feat: create user create or update validation

---
+++ internal/model/web/user_create_update_request.go
@@ -0,0 +1,13 @@
+package web
+
+type UserCreateRequest struct {
+	Email    string `validate:"required,email" json:"email"`
+	UserName string `validate:"required,max=200" json:"username"`
+	RoleId   int    `validate:"required" json:"role_id"`
+}
+
+type UserUpdateRequest struct {
+	Id       int    `validate:"required" json:"id"`
+	Email    string `validate:"required,email" json:"email"`
+	UserName string `validate:"required,max=200" json:"username"`
+}
