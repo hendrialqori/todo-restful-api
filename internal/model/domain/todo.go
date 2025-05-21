cd99d9bf11008c8a736eabb1d83fd23e9aac888b
Hendri Alqori <teamhendri18@gmail.com>
2025-05-21 17:31:33 +0700

    chore: remove json tag on todo & user struct

--- internal/model/domain/todo.go
+++ internal/model/domain/todo.go
@@ -1,12 +1,10 @@
 package domain
 
-import "time"
-
 type Todo struct {
-	Id        int       `json:"id"`
-	Title     int       `json:"title"`
-	Status    string    `json:"status"`
-	User      User      `json:"user"`
-	CreateAt  time.Time `json:"created_at"`
-	UpdatedAT time.Time `json:"updated_at"`
+	Id        int
+	Title     int
+	Status    string
+	UserId    int
+	CreatedAt string
+	UpdatedAt string
 }
--- internal/model/domain/user.go
+++ internal/model/domain/user.go
@@ -1,12 +1,11 @@
 package domain
 
-import "time"
-
 type User struct {
-	Id        int       `json:"id"`
-	Email     string    `json:"email"`
-	UserName  string    `json:"username"`
-	Role      Role      `json:"role"`
-	CreateAt  time.Time `json:"created_at"`
-	UpdatedAT time.Time `json:"updated_at"`
+	Id        int
+	Email     string
+	UserName  string
+	RoleId    int
+	Role      Role
+	CreatedAt string
+	UpdatedAt string
 }
