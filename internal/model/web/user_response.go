570db48fc32ae0aa5900f08036b2451a4d5091e7
Hendri Alqori <teamhendri18@gmail.com>
2025-05-22 16:56:20 +0700

    feat: add omitzero & omitempty json tag

--- internal/model/web/user_response.go
+++ internal/model/web/user_response.go
@@ -4,7 +4,7 @@ type UserResponse struct {
 	Id        int          `json:"id"`
 	Email     string       `json:"email"`
 	UserName  string       `json:"username"`
-	Role      RoleResponse `json:"role"`
-	CreatedAt string       `json:"created_at"`
-	UpdatedAt string       `json:"updated_at"`
+	Role      RoleResponse `json:"role,omitzero"`
+	CreatedAt string       `json:"created_at,omitempty"`
+	UpdatedAt string       `json:"updated_at,omitempty"`
 }
