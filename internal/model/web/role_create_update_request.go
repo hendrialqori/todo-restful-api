a99cb3a132e443f21daa49ec42ce7cd68711a99e
Hendri Alqori <teamhendri18@gmail.com>
2025-05-22 21:56:50 +0700

    feat: add example key to role create & update request

--- internal/model/web/role_create_update_request.go
+++ internal/model/web/role_create_update_request.go
@@ -1,10 +1,10 @@
 package web
 
 type RoleCreateRequest struct {
-	Name string `validate:"required,max=50,min=0" json:"name"`
+	Name string `validate:"required,max=50,min=0" json:"name" example:"SUPERADMIN"`
 }
 
 type RoleUpdateRequest struct {
 	Id   int    `validate:"required" json:"id"`
-	Name string `validate:"required,max=50,min=0" json:"name"`
+	Name string `validate:"required,max=50,min=0" json:"name" example:"ADMIN"`
 }
