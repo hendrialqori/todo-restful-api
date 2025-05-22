a286c52911f5ce763ddb57ddef3df73033bab1c0
Hendri Alqori <teamhendri18@gmail.com>
2025-05-22 21:56:22 +0700

    feat: add example key to role request

--- internal/model/web/api_response.go
+++ internal/model/web/api_response.go
@@ -1,8 +1,14 @@
 package web
 
 type ApiResponse struct {
-	Ok      bool   `json:"ok"`
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Ok      bool   `json:"ok" example:"true"`
+	Code    int    `json:"code" example:"200"`
+	Message string `json:"message" example:"success"`
 	Data    any    `json:"data"`
+}
+
+type NotFoundResponse struct {
+	Ok      bool   `json:"ok" example:"false"`
+	Code    int    `json:"code" example:"404"`
+	Message string `json:"message" example:"not_found_error"`
 }
